1881af5596c1604ef37714b55d693ee619aa79e0
stm33 <satuma33@icloud.com>
2022-12-04 01:24:29 +0900

    [add]refs #1 project-create

---
+++ main.ts
@@ -0,0 +1,17 @@
+const isOk: boolean = true;
+
+if (isOk) {
+    console.log("OK")
+}
+
+
+const nums: number[] = [1, 2, 3, 4]
+console.log(nums)
+
+const nums2: Array<number> = [5, 6, 7, 8]
+console.log(nums2)
+
+
+for (let num of nums) {
+    console.log(num)
+}
