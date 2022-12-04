1d973419fb0f716db8c7f835da3adbe42fa63c45
stm33 <satuma33@icloud.com>
2022-12-04 23:37:44 +0900

    [add]refs #3-fuction type declaration

--- main.ts
+++ main.ts
@@ -1,17 +1,48 @@
-const isOk: boolean = true;
+let person: {
+    name: string;
+    age: number;
+}
+person = {name: "aaa", age: 20};
 
-if (isOk) {
-    console.log("OK")
+console.log(person)
+
+type Car = {
+    readonly price: number;
+    manufacturer: string;
+    modelName: string
 }
 
+let car: Car = {
+    price: 1800000,
+    manufacturer: "TOYOTA",
+    modelName: "RandCruiser70",
+}
+/*
+const {price, manufacturer} = car;
+console.log(price, manufacturer)
 
-const nums: number[] = [1, 2, 3, 4]
-console.log(nums)
+const nums: number[] = [1, 2, 3, 4, 5]
+const [one, two, three, four, five, six] = nums;
+console.log(one, two, three, four, five, six)
 
-const nums2: Array<number> = [5, 6, 7, 8]
-console.log(nums2)
+for (let i = 0; i < nums.length; i++) {
+    console.log(nums[i]);
+}
 
-
-for (let num of nums) {
+for (const num of nums) {
     console.log(num)
 }
+ */
+
+
+const arr: number[] = [1, 2, 3]
+const arr2: number[] = [...arr, 4, 5]
+console.log(arr2)
+
+
+
+
+
+
+
+
