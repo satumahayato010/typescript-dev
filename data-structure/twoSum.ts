a58891e791d88c828049b0911f51e5410d94ecf4
stm33 <satuma33@icloud.com>
2023-01-09 20:56:44 +0900

    [add]refs #37-two-sum

---
+++ data-structure/twoSum.ts
@@ -0,0 +1,10 @@
+/*
+Two Sum
+整数numsの配列と整数targetが与えられたとき、
+2つの数値の足し算がtargetになるようなインデックスを返す。
+各入力は正確に1つの解を持つと仮定してよく、同じ要素を2度使ってはならない。
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
